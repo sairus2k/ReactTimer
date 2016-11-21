bde6255774999bcb197f4a9bc84e59b2383cd4e3
Anton Savoskin <anton@sliday.com>
2016-11-21 18:04:05 +0700

    Add automatic test

---
+++ karma.conf.js
@@ -0,0 +1,23 @@
+const webpackConfig = require('./webpack.config');
+
+module.exports = function (config) {
+  config.set({
+    browsers: ['Chrome'],
+    singleRun: true,
+    frameworks: ['mocha'],
+    files: ['app/tests/**/*.spec.jsx'],
+    preprocessors: {
+      'app/tests/**/*.spec.jsx': ['webpack', 'sourcemap']
+    },
+    reporters: ['mocha'],
+    client: {
+      mocha: {
+        timeout: '5000'
+      }
+    },
+    webpack: webpackConfig,
+    webpackServer: {
+      noInfo: true
+    }
+  });
+};
