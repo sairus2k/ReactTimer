4c6d207499a326039d5f3b0ea6bd1e56c66f1e05
Anton Savoskin <anton@sliday.com>
2016-11-22 22:19:37 +0700

    Finalyze Timer component

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -6,12 +6,12 @@ const Timer = React.createClass({
   getInitialState: function () {
     return {
       count: 0,
-      countdownStatus: 'stopped'
+      timerStatus: 'stopped'
     }
   },
   componentDidUpdate: function (prevProps, prevState) {
-    const status = this.state.countdownStatus;
-    if (status !== prevState.countdownStatus) {
+    const status = this.state.timerStatus;
+    if (status !== prevState.timerStatus) {
       const switchCase = {
         started: () => {
           this.startTimer();
@@ -30,6 +30,10 @@ const Timer = React.createClass({
       (switchCase[status] || switchCase['default'])();
     }
   },
+  componetWillUnmount: function () {
+    clearInterval(this.timer);
+    this.timer = null;
+  },
   startTimer: function () {
     this.timer = setInterval(() => {
       const newCount = this.state.count + 1;
@@ -40,16 +44,16 @@ const Timer = React.createClass({
   },
   handleStatusChange: function (newStatus) {
     this.setState({
-      countdownStatus: newStatus
+      timerStatus: newStatus
     });
   },
   render: function () {
-    const { count, countdownStatus } = this.state;
+    const { count, timerStatus } = this.state;
     return (
       <div>
         <h1 className="page-title">Timer App</h1>
         <Clock totalSeconds={count}/>
-        <Controls onStatusChange={this.handleStatusChange} countdownStatus={countdownStatus}/>
+        <Controls onStatusChange={this.handleStatusChange} countdownStatus={timerStatus}/>
       </div>
     )
   }
--- app/tests/components/Timer.spec.jsx
+++ app/tests/components/Timer.spec.jsx
@@ -6,8 +6,8 @@ const Timer = require('../../components/Timer');
 
 describe('Timer', () => {
   let clock;
-  
-  before(() =>{
+
+  before(() => {
     clock = new sinon.useFakeTimers();
   });
 
@@ -20,25 +20,28 @@ describe('Timer', () => {
     it('should start count and set state on start', () => {
       countdown.handleStatusChange('started');
       expect(countdown.state.count).toBe(0);
-      expect(countdown.state.countdownStatus).toBe('started');
+      expect(countdown.state.timerStatus).toBe('started');
       clock.tick(7001);
       expect(countdown.state.count).toBe(7);
     });
 
     it('should stop count and set state on pause', () => {
       countdown.handleStatusChange('paused');
+      clock.tick(2001);
       expect(countdown.state.count).toBe(7);
-      expect(countdown.state.countdownStatus).toBe('paused');
+      expect(countdown.state.timerStatus).toBe('paused');
     });
 
     it('should reset and set state on stop', () => {
+      countdown.setState({ count: 10 });
       countdown.handleStatusChange('stopped');
+      clock.tick(2001);
       expect(countdown.state.count).toBe(0);
-      expect(countdown.state.countdownStatus).toBe('stopped');
+      expect(countdown.state.timerStatus).toBe('stopped');
     });
   });
 
-  after(() =>{
+  after(() => {
     clock.restore();
   });
 });
